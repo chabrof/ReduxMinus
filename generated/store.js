56f0eb88a3b1aa67456e2649d32d22764e875d0c
François CHABROU <developer@stopgapjs.org>
2017-01-08 16:03:09 +0100

    Second version working on simple samples, addMiddleware is now a intellectual mess.

--- generated/store.js
+++ generated/store.js
@@ -4,9 +4,9 @@ define(["require", "exports"], function (require, exports) {
         function Store() {
             this._subscriberIdx = 0;
             this._subscriberCbks = [];
+            this._middlewares = [];
         }
         Store.prototype.getState = function () { return this._state; };
-        Store.prototype._unsubscribe = function (subscriberIdx) { this._subscriberCbks[subscriberIdx] = null; };
         Store.prototype.subscribe = function (subscriberCbk) {
             var _this = this;
             this._subscriberCbks[this._subscriberIdx] = subscriberCbk;
@@ -16,21 +16,40 @@ define(["require", "exports"], function (require, exports) {
             return ret;
         };
         Store.prototype.dispatch = function (action) {
-            this._state = this._globalReducer(this._state, action);
+            var _this = this;
+            var state = this._globalReducer(this._state, action);
+            console.log('state', state);
+            var next = function (action) { return state; };
+            this._middlewares.forEach(function (middleware) { return middleware(_this)(next); });
+            this._state = state;
             this._subscriberCbks.forEach(function (cbk) { if (cbk) {
                 cbk();
             } });
+            return action;
         };
-        Store.prototype._setState = function (state) { this._state = state; };
-        Store.prototype._setGlobalReducer = function (globalReducer) {
-            this._globalReducer = globalReducer;
-        };
+        Object.defineProperty(Store.prototype, "middlewares", {
+            set: function (middlewares) { this._middlewares = middlewares; },
+            enumerable: true,
+            configurable: true
+        });
+        Store.prototype.replaceReducer = function (reducer) { this._globalReducer = reducer; };
+        Store.prototype._unsubscribe = function (subscriberIdx) { this._subscriberCbks[subscriberIdx] = null; };
         return Store;
     }());
-    exports.store = new Store();
-    function createStore(globalReducer) {
-        exports.store._setGlobalReducer(globalReducer);
-        exports.store._setState(globalReducer());
+    function createStore(reducer, preloadedState, enhancer) {
+        if (typeof preloadedState === "function") {
+            enhancer = preloadedState;
+            preloadedState = undefined;
+        }
+        if (enhancer) {
+            var truc = enhancer(createStore);
+            return truc(reducer, preloadedState);
+        }
+        var store = new Store();
+        store.replaceReducer(reducer);
+        console.log('preload', reducer);
+        store.dispatch({ type: "@@redux/INIT" });
+        return store;
     }
     exports.createStore = createStore;
     function combineReducers(reducersObj) {
@@ -43,5 +62,20 @@ define(["require", "exports"], function (require, exports) {
         };
     }
     exports.combineReducers = combineReducers;
+    function applyMiddleware() {
+        var middlewares = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            middlewares[_i] = arguments[_i];
+        }
+        console.warn('APPly Middleware', middlewares);
+        return function (createStore) {
+            (function (reducer, preloadedState, enhancer) {
+                var store = createStore(reducer, preloadedState, enhancer);
+                store.middlewares = middlewares;
+                return store;
+            });
+        };
+    }
+    exports.applyMiddleware = applyMiddleware;
 });
 //# sourceMappingURL=store.js.map
--- src/store.ts
+++ src/store.ts
@@ -3,10 +3,8 @@ class Store {
   private _subscriberIdx :number = 0
   private _subscriberCbks :(() => any)[] = []
   private _globalReducer :(state? :any, action? :any) => any
+  private _middlewares :((store :Store) => any)[] = []
   public getState() :any { return this._state }
-
-  private _unsubscribe(subscriberIdx) { this._subscriberCbks[subscriberIdx] = null }
-
   public subscribe(subscriberCbk :() => any) :() => any {
     this._subscriberCbks[this._subscriberIdx] = subscriberCbk
     let closSubscriberIdx = this._subscriberIdx
@@ -14,29 +12,48 @@ class Store {
     this._subscriberIdx++
     return ret
   }
-
   public dispatch(action :any) {
-    this._state = this._globalReducer(this._state, action)
-    this._subscriberCbks.forEach((cbk) => { if (cbk) { cbk(); } })
+    let state = this._globalReducer(this._state, action)
+    console.log('state', state)
+    let next = (action) => state
+    this._middlewares.forEach((middleware) => { return middleware(this)(next) })
+    this._state = state
+    this._subscriberCbks.forEach((cbk) => { if (cbk) { cbk() } })
+    return action
   }
-
-  public _setState(state :any) { this._state = state }
-  public _setGlobalReducer(globalReducer :(state? :any, action? :any) => any) {
-    this._globalReducer = globalReducer
+  set middlewares(middlewares :((store) => any)[]) { this._middlewares = middlewares }
+  public replaceReducer(reducer :(state? :any, action? :any) => any) { this._globalReducer = reducer }
+  private _unsubscribe(subscriberIdx) { this._subscriberCbks[subscriberIdx] = null }
+}
+export function createStore(reducer :(state? :any, action? :any) => any, preloadedState :any, enhancer :(createStore) => any) {
+  if (typeof preloadedState === "function") {
+    enhancer = preloadedState
+    preloadedState = undefined;
   }
+  if (enhancer) {
+    let truc = enhancer(createStore);
+    return truc(reducer, preloadedState)
+  }
+  let store = new Store()
+  store.replaceReducer(reducer)
+  console.log('preload',  reducer)
+  store.dispatch({ type :"@@redux/INIT"});
+  return store
 }
-
-export let store = new Store(); // singleton
-
-export function createStore(globalReducer :(state? :any, action? :any) => any) {
-  store._setGlobalReducer(globalReducer)
-  store._setState(globalReducer())
-}
-
 export function combineReducers(reducersObj :{[name :string]:() => any}) :(state? :any, action? :any) => any {
   return function(state? :any, action? :any) {
     let initialState :any = {}
     for (let i in reducersObj) { initialState[i] = reducersObj[i]() }
     return initialState
+  }
+}
+export function applyMiddleware(...middlewares :((store :Store) => any)[]) {
+  console.warn('APPly Middleware', middlewares);
+  return (createStore) => {
+    (reducer, preloadedState, enhancer) => {
+      let store :Store = createStore(reducer, preloadedState, enhancer)
+      store.middlewares = middlewares
+      return store
+    }
   }
 }
